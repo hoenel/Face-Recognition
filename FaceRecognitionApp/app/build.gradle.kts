f9bb809cbed9ac122a3db5ed68515cf215ffe6df
Trungkun103 <trungkun103@gmail.com>
2025-08-26 01:05:28 +0700

    checked

--- FaceRecognitionApp/app/build.gradle.kts
+++ FaceRecognitionApp/app/build.gradle.kts
@@ -39,35 +39,40 @@ android {
 
 
 dependencies {
-    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-auth:22.3.1")
-    implementation ("com.google.firebase:firebase-firestore")
-    implementation ("com.google.firebase:firebase-database")
+    // Sử dụng Firebase BoM để quản lý các phiên bản
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
 
+    // Thư viện Firebase không cần chỉ định phiên bản
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    // Thư viện TF Lite và CameraX
     implementation("org.tensorflow:tensorflow-lite:2.14.0")
     implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
     implementation("org.tensorflow:tensorflow-lite-select-tf-ops:2.14.0")
     implementation("org.tensorflow:tensorflow-lite-metadata:0.4.4")
 
     val camerax_version = "1.3.1"
+    implementation("androidx.camera:camera-core:$camerax_version")
+    implementation("androidx.camera:camera-camera2:$camerax_version")
+    implementation("androidx.camera:camera-lifecycle:$camerax_version")
+    implementation("androidx.camera:camera-view:$camerax_version")
+    implementation("androidx.camera:camera-extensions:$camerax_version")
 
-    implementation ("androidx.camera:camera-core:$camerax_version")
-    implementation ("androidx.camera:camera-camera2:$camerax_version")
-    implementation ("androidx.camera:camera-lifecycle:$camerax_version")
-    implementation ("androidx.camera:camera-view:$camerax_version")
-    implementation ("androidx.camera:camera-extensions:$camerax_version")
+    // Thư viện datastore bạn đã thêm
+    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
 
+    // Các thư viện khác
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.database)
-    implementation(libs.camera.view)
-    implementation(libs.camera.lifecycle)
     implementation(libs.vision.common)
     implementation(libs.play.services.mlkit.face.detection)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
